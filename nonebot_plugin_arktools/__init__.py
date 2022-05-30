3c6ac0262b24ec5ccc89cd691929c9ce77f6236e
Number_Sir <52584526+NumberSir@users.noreply.github.com>
2022-05-30 14:33:03 +0800

    Update __init__.py

--- nonebot_plugin_arktools/__init__.py
+++ nonebot_plugin_arktools/__init__.py
@@ -2,8 +2,3 @@ import nonebot
 from .ark_daily_source import *
 from .activity_notice import *
 from ._utils import *
-
-driver = nonebot.get_driver()
-@driver.on_startup
-async def _():
-    nonebot.load_plugins("./nonebot_plugin_arktools")
