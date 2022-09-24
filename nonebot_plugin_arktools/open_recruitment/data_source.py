ff36a00cc456e06d907918cdd85906088b863004
Number_Sir <number_sir@126.com>
2022-09-24 16:47:30 +0800

    修复干员公招相关问题

--- nonebot_plugin_arktools/open_recruitment/data_source.py
+++ nonebot_plugin_arktools/open_recruitment/data_source.py
@@ -3,6 +3,7 @@ from .config import Config
 from nonebot import get_driver
 from nonebot import logger
 from dataclasses import dataclass
+from itertools import permutations
 
 import json
 from tencentcloud.common import credential
@@ -11,6 +12,7 @@ from tencentcloud.ocr.v20181119 import ocr_client, models
 from pathlib import Path
 from PIL import Image, ImageFont
 from PIL.ImageDraw import Draw, ImageDraw
+import math
 
 recruit_config = Config.parse_obj(get_driver().config.dict())
 SAVE_PATH = Path(recruit_config.recruitment_save_path)
@@ -58,144 +60,93 @@ def load_operator_data() -> dict:
     operators = {}
     with open(Path(__file__).parent.parent / "_data" / "operator_info" / "json" / "character_table.json", "r", encoding="utf-8") as f:
         operator_basic_info: dict = json.load(f)
-
     with open(Path(__file__).parent.parent / "_data" / "operator_info" / "json" / "handbook_info_table.json", "r", encoding="utf-8") as f:
         operator_data_info: dict = json.load(f)
-
     with open(Path(__file__).parent.parent / "_data" / "operator_info" / "json" / "gacha_table.json", "r", encoding="utf-8") as f:
         operator_obtainable: dict = json.load(f)
-
     for code, info in operator_basic_info.items():
-        if not info["itemObtainApproach"]:
+        text = operator_obtainable["recruitDetail"]
+        text = text.replace("\\n", "\n").replace("<@rc.eml>", "\n").replace("</>", "\n").split("\n")
+
+        text = [_ for _ in text if _ and "<" not in _ and "--" not in _ and "★" not in _ and _ != " / "][1:]
+
+        text = [" ".join(_.split(" / ")) for _ in text]
+        text = " ".join(_.strip() for _ in text).split()
+        if info["name"] not in text:
             continue
-        if info["itemObtainApproach"] != "招募寻访":
+        if not info["itemObtainApproach"]:
             continue
         if info["isSpChar"]:
             continue
-        if info["name"] not in operator_obtainable["recruitDetail"]:
-            continue
-        operator = Operator(
-            code=code,
-            name=info["name"],
-            prof=info["profession"],
-            rarity=info["rarity"],
-            tags=info["tagList"],
-            pos=info["position"]
-        )
-        operators[code] = operator
+        operator = Operator(code=code, name=info["name"], prof=info["profession"], rarity=info["rarity"], tags=info["tagList"], pos=info["position"])
 
+        if operator.prof == "PIONEER":
+            operator.tags.append("先锋干员")
+        elif operator.prof == "WARRIOR":
+            operator.tags.append("近卫干员")
+        elif operator.prof == "SNIPER":
+            operator.tags.append("狙击干员")
+        elif operator.prof == "MEDIC":
+            operator.tags.append("医疗干员")
+        elif operator.prof == "TANK":
+            operator.tags.append("重装干员")
+        elif operator.prof == "SUPPORT":
+            operator.tags.append("辅助干员")
+        elif operator.prof == "CASTER":
+            operator.tags.append("术师干员")
+        else:
+            operator.tags.append("特种干员")
+        if operator.pos == "MELEE":
+            operator.tags.append("近战位")
+        else:
+            operator.tags.append("远程位")
+        if operator.rarity == 5:
+            operator.tags.append("高级资深干员")
+        elif operator.rarity == 4:
+            operator.tags.append("资深干员")
+        elif operator.rarity == 0:
+            operator.tags.append("支援机械")
+        operators[code] = operator
     for code, info in operator_data_info["handbookDict"].items():
         if code not in operators:
             continue
-
         if "男" in info["storyTextAudio"][0]["stories"][0]["storyText"]:
             operators[code].sex = 1
-
     return operators
 
 
 def get_rare_operators(tags: set) -> list:
-    """
-    获取干员
-    性别 -> 男/女
-    职业 -> 先锋/近卫/重装/狙击/术师/医疗/辅助/特种
-    位置 -> 近战/远程
-    高资 -> 六星
-    资深 -> 五星
-    """
-    profession = {}
-    position = {}
-    sex = {}
-    rarity = {0: 0, 1: 1, 2: 2, 3: 3, 4: 4}
-    general = {}
-
-    with open(Path(__file__).parent.parent / "_data" / "operator_info" / "json" / "recruitment_tags.json", "r", encoding="utf-8") as f:
-        tags_cate = json.load(f)
-
+    """获取干员"""
     operators = load_operator_data()
+    combinations = build_combinations(tags)
+    result = []
+    for comb in combinations:
+        comb = sorted(list(comb))
+        mapping = []
+        for code, data in operators.items():
+            if len(comb) <= len(data.tags) and set(comb).issubset(data.tags):
+                if "高级资深干员" not in comb and data.rarity == 5:
+                    continue
+                if (data.name, data.code, data.rarity) in mapping:
+                    continue
+                mapping.append((data.name, data.code, data.rarity))
+        mapping = sorted(mapping)
+        if mapping and {"tags": comb, "operators": mapping} not in result:
+            result.append({"tags": comb, "operators": mapping})
 
-    for tag in tags:
-        if tag in tags_cate["资质"]:
-            if tag == "新手":
-                rarity[1] = "1"
-            elif tag == "资深干员":
-                rarity[4] = tag
-            elif tag == "高级资深干员":
-                rarity[5] = tag
-            else:
-                rarity[0] = tag
-        elif tag in tags_cate["性别"]:
-            if tag == "男性干员":
-                sex[1] = tag
-            else:
-                sex[0] = tag
-        elif tag in tags_cate["位置"]:
-            if tag == "近战位":
-                position["MELEE"] = tag
-            else:
-                position["RANGED"] = tag
-        elif tag in tags_cate["职业"]:
-            if tag == "先锋干员":
-                profession["PIONEER"] = tag
-            elif tag == "医疗干员":
-                profession["MEDIC"] = tag
-            elif tag == "术师干员":
-                profession["CASTER"] = tag
-            elif tag == "特种干员":
-                profession["SPECIAL"] = tag
-            elif tag == "狙击干员":
-                profession["SNIPER"] = tag
-            elif tag == "辅助干员":
-                profession["SUPPORT"] = tag
-            elif tag == "近卫干员":
-                profession["WARRIOR"] = tag
-            elif tag == "重装干员":
-                profession["TANK"] = tag
-        else:
-            general[tag] = tag
+    result_ = []
+    for r in result:
+        flag = all(op[2] not in {1, 2} for op in r["operators"])
+        if flag:
+            result_.append(r)
+    return result_
 
-    result = {}
-    for code, op in operators.items():
-        if op.rarity in rarity:
-            if op.code in result:
-                continue
-            result[op.code] = {"code": op.code, "name": op.name, "rarity": op.rarity, "tags": [str(rarity[op.rarity])]}
 
-            if op.prof in profession:
-                result[op.code]["tags"].append(profession[op.prof])
-            if op.pos in position:
-                result[op.code]["tags"].append(position[op.pos])
-            if op.sex in sex:
-                result[op.code]["tags"].append(sex[op.sex])
-
-        for t in op.tags:
-            if t in general and op.rarity in rarity:
-                result[op.code]["tags"].append(general[t])
-
-    # pprint(result)
-    result_list = []
-    for _, __ in result.copy().items():
-        tags = sorted([t for t in __["tags"] if t not in {"0", "1", "2", "3", "4"}])
-        if not tags:  # 剔除占位用
-            continue
-
-        mapping = [
-            (data["name"], data["code"], data["rarity"])
-            for code, data in result.copy().items()
-            if sorted([
-                t
-                for t in data["tags"]
-                if t not in {"0", "1", "2", "3", "4"}
-            ]) == tags
-        ]
-
-        if {"tags": tags, "operators": mapping} in result_list:
-            continue
-
-        if not any(any(i in m for i in {1, 2}) for m in mapping):
-            result_list.append({"tags": tags, "operators": mapping})
-
-    return result_list
+def build_combinations(tags: set) -> set:
+    result = []
+    for i in range(1, 6):
+        result += list(permutations(tags, i))
+    return set(result)
 
 
 def text_border(text: str, draw: ImageDraw, x: int, y: int, font: ImageFont, shadow_colour: tuple, fill_colour: tuple, anchor: str = "la"):
@@ -218,66 +169,111 @@ def build_image(result_list: list) -> Image:
     if not result_list:
         return None
 
-    main_background = Image.new(mode="RGBA", size=(2002, 3360), color=(50, 50, 50, 255))
-    pri_height = 0
-    pri_width = 0
+    # 先构造每一个tag组
+    result_imgs = []
+    for result in result_list:
+        """这是一个tag组的结果"""
+        rows = math.ceil(len(result["operators"]) / 6)
+        result_bg = Image.new("RGBA", (920, 152 * rows), (0, 0, 0, 0))
+        """152x152的TAG, 128x(128+24)的头图"""
+
+        tag_bg = Image.new("RGBA", (152, 152), (50, 50, 50, 0))
+        for idx, tag in enumerate(result["tags"]):
+            """一个tag组里的每个tag"""
+            text_border(text=tag, draw=Draw(tag_bg), x=76, y=(15 + 30 * idx), anchor="mm", font=font, fill_colour=(255, 255, 255, 255), shadow_colour=(0, 0, 0, 255))
+        result_bg.paste(tag_bg, box=(0, 0), mask=tag_bg.split()[3])
+
+        op_bg = Image.new("RGBA", (920, 912), (50, 50, 50, 0))
+        font_op = ImageFont.truetype(str(FONT_PATH / "Arknights-zh.otf"), 20)
+        for idx, op_tuple in enumerate(result["operators"]):
+            """一个tag组里的所有干员信息"""
+            avatar = Image.open(Path(__file__).parent.parent / "_data" / "operator_info" / "image" / "avatar" / f"{op_tuple[1]}.png").convert("RGBA").resize((128, 128))  # 头像
+            # 贴个对应颜色的光
+            avatar_bg = None
+            if op_tuple[2] == 5:  # 六星
+                avatar_bg = Image.new("RGBA", (128, 128), (255, 127, 39, 250))
+                avatar_bg.paste(avatar, mask=avatar.split()[3])
+            elif op_tuple[2] == 4:  # 五星
+                avatar_bg = Image.new("RGBA", (128, 128), (255, 201, 14, 250))
+                avatar_bg.paste(avatar, mask=avatar.split()[3])
+            elif op_tuple[2] == 3:  # 四星
+                avatar_bg = Image.new("RGBA", (128, 128), (216, 179, 216, 250))
+                avatar_bg.paste(avatar, mask=avatar.split()[3])
+            elif op_tuple[2] == 0:  # 一星
+                avatar_bg = Image.new("RGBA", (128, 128), (255, 255, 255, 250))
+                avatar_bg.paste(avatar, mask=avatar.split()[3])
+
+            if avatar_bg:
+                op_bg.paste(avatar_bg, box=(128 * (idx - 6 * (idx // 6)), 0 + 152 * (idx // 6)),  mask=avatar_bg.split()[3])  # 粘头像，每六个换行
+            text_border(op_tuple[0], Draw(op_bg), x=(64 + 128 * (idx - 6 * (idx // 6))), y=(128 + 12 + 152 * (idx // 6)), anchor="mm", font=font_op, fill_colour=(255, 255, 255, 255), shadow_colour=(0, 0, 0, 255))
+        result_bg.paste(op_bg, box=(152, 0), mask=op_bg.split()[3])
+        result_imgs.append((result_bg, rows))  # 记录每个结果及行数，方便绘制总图
+
+    # 分组，每列十行
+    result_combs: list = process_result_imgs(result_imgs)
+    columns = len(result_combs)  # 总列数
+
+    """绘制总图，最多十行 -> 152*10 + 24*2"""
+    main_background = Image.new("RGBA", size=(10 * 2 + (920 + 24) * columns, 10 * 2 + 152 * 10), color=(50, 50, 50, 200))
+    width = main_background.size[0]
+    height = main_background.size[1]
+
+    Draw(main_background).rectangle(xy=(0, 0, width, height), outline=(200, 200, 200), width=10)
+
     H = 0
+    for idx_column, comb in enumerate(result_combs):  # 每列
+        H = 0
+        if idx_column != 0:
+            Draw(main_background).line(xy=(10 + (920 + 24) * idx_column, 0, 10 + (920 + 24) * idx_column, height), width=4, fill=(200, 200, 200))
+        for idx_row, img_tuple in enumerate(comb):  # 每行
+            if idx_row != 0:
+                Draw(main_background).line(xy=(10 + (920 + 24) * idx_column, 10 + H, 944 + 10 + (920 + 24) * idx_column, 10 + H), width=4, fill=(200, 200, 200))
+            main_background.paste(im=img_tuple[0], box=(10 + (920 + 24) * idx_column, 10 + H), mask=img_tuple[0].split()[3])
+            H += img_tuple[0].size[1]
 
-    for data in result_list:
-        L = len(data["operators"])
-        tag_background = Image.new(mode="RGBA", size=(956, 168 * (L // 6 + 1)), color=(100, 100, 100, 0))  # TAG + 6干员图
-        tags = data["tags"]
-        L = len(tags)
-        tag_bg = Image.new(mode="RGBA", size=(144, 168), color=(0, 0, 0, 0))
-        for tag_idx, tag in enumerate(tags):
-            text_border(text=tag, draw=Draw(tag_bg), x=72, y=102 - 18 * L + 36 * tag_idx, anchor="mm", font=font, shadow_colour=(0, 0, 0, 255), fill_colour=(255, 255, 255, 255))
-        tag_background.paste(tag_bg, box=(0, 0), mask=tag_bg.split()[3])
+    if len(result_combs) == 1 and 0 < H < height-10:  # 只有一列，可能要裁图片
+        main_background = main_background.crop(box=(0, 0, width, H+16))
+        Draw(main_background).line(xy=(0, H+10, width, H+10), width=10, fill=(200, 200, 200))
 
-        for op_idx, op in enumerate(data["operators"]):
-            op_background = Image.new(mode="RGBA", size=(128, 164), color=(0, 0, 0, 0))  # 干员头图+名称
-            avatar = Image.open(Path(__file__).parent.parent / "_data" / "operator_info" / "image" / "avatar" / f"{op[1]}.png").convert("RGBA").resize((128, 128))  # 头像
-            name = op[0]
-            op_background.paste(im=avatar, mask=avatar.split()[3])
-            text_border(text=op[0], draw=Draw(op_background), x=64, y=150, anchor="mm", font=font, shadow_colour=(0, 0, 0, 255), fill_colour=(255, 255, 255, 255))
-            tag_background.paste(im=op_background, box=(168 + 132 * (op_idx % 6), 2 + 168 * (op_idx // 6)))
-
-        if pri_height == 0:
-            if pri_width != 0:
-                main_background.paste(im=tag_background, mask=tag_background.split()[3], box=(pri_width + 24, 24))
-            else:
-                main_background.paste(im=tag_background, mask=tag_background.split()[3], box=(24, 24))
-        elif pri_width != 0:
-            main_background.paste(im=tag_background, mask=tag_background.split()[3], box=(pri_width + 24, pri_height + 42))
-        else:
-            main_background.paste(im=tag_background, mask=tag_background.split()[3], box=(24, pri_height + 42))
-        pri_height += 24 + tag_background.size[1]
-        if pri_height > 1200:
-            H = pri_height
-            pri_width = 998
-            pri_height = 0
-
-        draw = Draw(main_background)
-        if pri_width == 0:
-            draw.line(xy=(0, pri_height + 21, 1001 - 2, pri_height + 21))
-        elif pri_height != 0:
-            draw.line(xy=(1001 - 2, pri_height + 21, 2002, pri_height + 21))
-
-    h = max(H, pri_height) + 24
-
-    if pri_width != 0:
-        main_background = main_background.crop((0, 0, 2002, h))
-    else:
-        main_background = main_background.crop((0, 0, 1004, h))
-    draw = Draw(main_background)
-    draw.line(xy=(0, 0, main_background.size[0], 0), fill=(190, 190, 190, 255), width=4)
-    draw.line(xy=(0, 0, 0, h), fill=(190, 190, 190, 255), width=4)
-    draw.line(xy=(main_background.size[0] - 2, 0, main_background.size[0] - 2, h), fill=(190, 190, 190, 255), width=4)
-    draw.line(xy=(0, h - 2, main_background.size[0] - 2, h - 2), fill=(190, 190, 190, 255), width=4)
-
-    draw.line(xy=(1001 - 2, 0, 1001 - 2, h - 2), fill=(190, 190, 190, 255), width=2)
     file = SAVE_PATH / "temp.png"
     None if os.path.exists(SAVE_PATH) else os.makedirs(SAVE_PATH)
     main_background.save(file)
     return file
 
-    
+
+def process_result_imgs(result_imgs: list):
+    # 先从大到小排列
+    # 然后一一配组
+    result_counts = sorted(result_imgs, key=lambda tup: tup[1], reverse=True)
+    already_in = []
+    comb = []
+    flag = False
+    for idx1, count1 in enumerate(result_counts):
+        if idx1 in already_in:
+            continue
+        tmp_count = [count1[1]]
+        tmp_idx = [idx1]
+        for idx2, count2 in enumerate(result_counts[idx1+1:]):
+            flag = False
+            if idx2+idx1+1 in already_in:
+                continue
+            if count1[1] + count2[1] > 10:
+                continue
+            elif count1[1] + count2[1] == 10:
+                already_in += [idx1, idx2+idx1+1]
+                comb.append([idx1, idx2+idx1+1])
+                break
+            if sum(tmp_count) + count2[1] > 10:
+                continue
+            elif sum(tmp_count) + count2[1] == 10:
+                already_in += [idx1, idx2+idx1+1]
+                comb.append(tmp_idx + [idx2+idx1+1])
+                break
+            tmp_idx.append(idx2+idx1+1)
+            already_in += [idx1, idx2+idx1+1]
+            tmp_count.append(count2[1])
+            flag = True
+        if flag:
+            comb.append(tmp_idx)
+
+    return [[result_counts[idx] for idx in c] for c in comb]
