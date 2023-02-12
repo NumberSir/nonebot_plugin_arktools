ec6eaf2374e045b86e08c3b9a792a7a9250a9fbb
Number_Sir <number_sir@126.com>
2023-02-13 03:34:41 +0800

    fix config

--- nonebot_plugin_arktools/src/configs/ocr_config.py
+++ nonebot_plugin_arktools/src/configs/ocr_config.py
@@ -3,8 +3,8 @@ from pydantic import BaseModel, Extra
 
 class BaiduOCRConfig(BaseModel, extra=Extra.ignore):
     """公招识别相关配置"""
-    arknights_baidu_app_id: str
-    arknights_baidu_api_key: str
+    arknights_baidu_app_id: str = ""
+    arknights_baidu_api_key: str = ""
 
 
 __all__ = [
